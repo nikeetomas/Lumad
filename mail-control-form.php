a86d0a51180d751c251a4be26efebafe67b626da
nikeetomas <71626991+nikeetomas@users.noreply.github.com>
2020-12-28 22:07:33 +0800

    Add files via upload

---
+++ mail-control-form.php
@@ -0,0 +1,28 @@
+<?php 
+
+		$name = $_POST['name'];
+		$visitor_email = $_POST['email'];
+		$message = $_POST['message'];
+
+		$email_from = 'k.developer.x@gmail.com';
+
+		$email_subject = 'New Form Query!';
+
+		$email_body = "Visitor Name : $name.\n".
+						"Visitor Email : $visitor_email.\n".
+							"Visitor Message : $message.\n";
+
+
+	 $to ="nikeetomas55@gmail.com";
+
+	 $headers = "Form : $email_from \r\n";
+	 $headers .= "Reply-To : $visitor_email \r\n";
+
+	$run = mail($to,$email_subject,$email_body,$headers);
+
+    if($run)
+	 	echo "<script>alert('Form Submitted!');location <a href='index.html';</script>";
+
+
+
+ ?>
